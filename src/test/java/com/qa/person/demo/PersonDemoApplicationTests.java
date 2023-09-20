cdf281e66a2818040ebbda18a5e55621104f84e9
jharry444 <jharrison1@qa.com>
2023-09-20 14:24:31 +0100

    Profiles Example

--- src/test/java/com/qa/person/demo/PersonDemoApplicationTests.java
+++ src/test/java/com/qa/person/demo/PersonDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.qa.person.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonDemoApplicationTests {
 
 //	context -> where the beans go
